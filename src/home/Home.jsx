30a66d78850b4a83d18b83213a4fbe9c8cf57836
odiljondilshodbekov2013@gmail.com <pop315306@gmail.com>
2026-08-15 11:44:44 +0500

    Add modern beautiful design to Home profile card

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -4,17 +4,31 @@ import image from '../img/myimage.jpg'
 import telegram from '../img/telegram.jpg'
 import youtube from '../img/youtube.png'
 
-
 const Home = () => {
   return (
-    <>
-  <img src={image} alt="" />
-  <h1>Odiljon</h1>
-  <p>Mening ismim Odiljon men asaka tumanidagi ixtisoslashgan maktabiga topshirib 7-o'rinda kirdim.</p>
-  <a href="https://www.youtube.com/@aisehri"><img src={youtube} alt="" /></a>
-  <a href="https://t.me/it_ai_faktlar"><img src={telegram} alt="" /></a>
-
-    </>
+    <div className="profile-container">
+      <div className="profile-card">
+        <div className="profile-image-wrapper">
+          <img className="profile-image" src={image} alt="Odiljon" />
+          <div className="status-indicator"></div>
+        </div>
+        
+        <h1 className="profile-name">Odiljon</h1>
+        
+        <p className="profile-bio">
+          Mening ismim Odiljon, men Asaka tumanidagi ixtisoslashgan maktabiga topshirib 7-o'rinda kirdim.
+        </p>
+        
+        <div className="social-links">
+          <a href="https://www.youtube.com/@aisehri" className="social-link youtube" target="_blank" rel="noreferrer">
+            <img src={youtube} alt="YouTube" />
+          </a>
+          <a href="https://t.me/it_ai_faktlar" className="social-link telegram" target="_blank" rel="noreferrer">
+            <img src={telegram} alt="Telegram" />
+          </a>
+        </div>
+      </div>
+    </div>
   )
 }
 
